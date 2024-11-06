2ed0f1352bb0ceaf13d76e673d2b52021e0d515a
Tom Rib <tom.ribero@laplateforme.io>
2024-11-06 14:30:13 +0100

    Fonction Tourbot

---
+++ tour_joueur.py
@@ -0,0 +1,5 @@
+# Tour du joueur humain
+def tour_joueur(grille, joueur):
+    position = int(input(f"Joueur {joueur}, choisissez une case (1-9): ")) - 1
+    if grille[position] == " ":
+        grille[position] = joueur
